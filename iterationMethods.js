e25a05c333800cbccf0b33a18c588c3038d8b454
ousef777 <yousef.alfeeli@gmail.com>
2024-11-04 08:11:09 +0300

    Solution

--- iterationMethods.js
+++ iterationMethods.js
@@ -6,16 +6,19 @@ const temperatures = [
 // 1) Write a `filterHighTemperatures` function that returns a new array containing temperatures greater than or equal to 25 degrees Celsius.
 function filterHighTemperatures(temps) {
   // Your code here
+  return temps.filter((value, index, array) => value >= 25);
 }
 
 // Write a `filterLowTemperatures` function that returns a new array containing temperatures less than 20 degrees Celsius.
 function filterLowTemperatures(temps) {
   // Your code here
+  return temps.filter((value, index, array) => value < 20);
 }
 
 // Write a `convertCelsiusToFahrenheit` function that converts the entire list of temperatures to Fahrenheit, and return it. Use the formula (Celsius * 9/5) + 32.
 function convertCelsiusToFahrenheit(temps) {
   // Your code here
+  return temps.map((value, index, array) => (value * 9 / 5) + 32);
 }
 
 // Write a `labelTemperatures` function that returns a new array that labels each temperature as either:
@@ -24,16 +27,24 @@ function convertCelsiusToFahrenheit(temps) {
 // "Cool" for temperatures <20
 function labelTemperatures(temps) {
   // Your code here
+  function mark(value, index, array) {
+    if (value >= 25) return "Warm";
+    if (value >= 20) return "Mild";
+    else return "Cool";
+  }
+  return temps.map(mark);
 }
 
 // Write a `getMaxTemperature` function that calculates and return the highest temperature of the month.
 function getMaxTemperature(temps) {
   // Your code here
+  return Math.max.apply(null, temps);
 }
 
 // Write a `getMinTemperature` function that calculates and return the lowest temperature of the month.
 function getMinTemperature(temps) {
   // Your code here
+  return Math.min.apply(null, temps);
 }
 
 module.exports = {
